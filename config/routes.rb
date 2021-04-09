51d68436603a4b872bc30e1b9e8e2471af1eb6c5
misa kohata <samisami1215@icloud.com>
2021-04-10 04:22:40 +0900

    コンタクトページ作成
    ビュー・ルート・スタイルシートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'portforio/profiile'
   get 'portforio/report'
   get 'portforio/work'
+  get 'portforio/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
