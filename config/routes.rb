d0154bb6155f15ccc95e18240a28a895ddf8234b
misa kohata <samisami1215@icloud.com>
2021-04-10 04:13:19 +0900

    workページ作成
    ビュー・ルート・スタイルシートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'portforio/top'
   get 'portforio/profiile'
   get 'portforio/report'
+  get 'portforio/work'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
