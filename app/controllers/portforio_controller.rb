3ddbd1403444fefe13b1022eeda54b878324ca11
misa kohata <samisami1215@icloud.com>
2021-04-12 04:50:22 +0900

    コントローラ・ルーティングの指定
    コントローラは扱いやすくなるように、パス名を変更。ルーティンは新規ファイルのアクションを指定していなかったため、追加した。（トップページ以外）

--- app/controllers/portforio_controller.rb
+++ app/controllers/portforio_controller.rb
@@ -1,4 +1,12 @@
 class PortforioController < ApplicationController
   def top
   end
+  def profiile
+  end
+  def report
+  end
+  def work
+  end
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'portforio/top'
-  get 'portforio/profiile'
-  get 'portforio/report'
-  get 'portforio/work'
-  get 'portforio/contact'
+  get 'portforio/top', to: 'portforio#top', as: '/'
+  get 'portforio/profiile', to: 'portforio#profile' , as: 'profile'
+  get 'portforio/report', to: 'portforio#report' , as: 'report'
+  get 'portforio/work', to: 'portforio#work' , as: 'work'
+  get 'portforio/contact', to: 'portforio#contact' , as: 'contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
