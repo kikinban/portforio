3cfbc93421ce6c0a893e1582325fb6c1c8293f02
misa kohata <samisami1215@icloud.com>
2021-04-12 05:28:41 +0900

    ルーティングの記述内容変更
    urlの名前を変える為には今回の記述方法で変更する必要があった。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'portforio/top', to: 'portforio#top', as: '/'
-  get 'portforio/profiile', to: 'portforio#profile' , as: 'profile'
-  get 'portforio/report', to: 'portforio#report' , as: 'report'
-  get 'portforio/work', to: 'portforio#work' , as: 'work'
-  get 'portforio/contact', to: 'portforio#contact' , as: 'contact'
+  get "/" => "portforio#top"
+  get "/profile" => "portforio#profile"
+  get "/report" => "portforio#report"
+  get "/work" => "portforio#work"
+  get "/contact" => "portforio#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
