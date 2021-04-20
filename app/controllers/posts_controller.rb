43ac1c44353bfba766c6728e928491824883027f
misa kohata <samisami1215@icloud.com>
2021-04-20 15:15:12 +0900

    入力フォーム作成
    form_withに付与するモデル名エラーが出る。様々な解決方法を試す。urlを付与することで、解決した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,25 @@
+
+# todo モデル名・コントローラ名・テーブル名を変更（study records）
 class PostsController < ApplicationController
   def index
-    @post = Post.all
+    @posts = Post.all
   end
   def new
+    @post = Post.new
   end
   def create
-    @post = Post.new(learning_content: params[:learning_content])
-    @post.save
-    redirect_to("/posts/index")
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to "/posts/index"
+    else
+      @post = Post.new(post_params)
+      render "posts/new"
+    end
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:time, :learning_plan, :learning_content, :it_took_time, :something_felt, :notices, :tomorrow)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    # ---- バリデーション設定 ----
+    validates :time, :learning_plan, :learning_content, :it_took_time, :something_felt, :notices, :tomorrow,
+    {presence: true}
+   
 end
